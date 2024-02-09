f1262aa770316a990123e4669f70acfa3d1a9fc7
Bhumikachauhan26 <159519180+Bhumikachauhan26@users.noreply.github.com>
2024-02-09 12:57:49 +0530

    Add files via upload

---
+++ bhumika/sticky.js
@@ -0,0 +1,11 @@
+function hamo(){
+   resnav = document.getElementById("resnavo").style.display="none";
+   navi = document.getElementById("navi").style.display="inline-block";
+   resnavc = document.getElementById("resnavc").style.display="inline-block";
+}
+
+function hamc(){
+   resnavc = document.getElementById("resnavc").style.display="none";
+   navi = document.getElementById("navi").style.display="none";
+   resnav = document.getElementById("resnavo").style.display="inline-block";
+}
